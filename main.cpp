ea7b99b0c42a56eb8a4fb4c09718858907599dc2
Munkhtemuulen Khashbolor <munhtemuulen.kh2002@gmail.com>
2024-10-21 09:50:32 +0900

    cpp project start

---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>  // This is the header file for input/output operations
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;  // Output the text to the console
+    return 0;  // Return 0 to indicate that the program ended successfully
+}
